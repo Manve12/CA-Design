3eaa05f4f50a26f92bfb784cb90f87e18b8be34c
manvidas <manvidas@NM-16010-PC>
2018-10-23 09:21:40 +0100

    Added chart to the index

--- GraphTestApplication2/GraphTestApplication2/Controllers/HomeController.cs
+++ GraphTestApplication2/GraphTestApplication2/Controllers/HomeController.cs
@@ -1,30 +1,41 @@
-﻿using System;
+﻿using GraphTestApplication2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace GraphTestApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
+            string imageBase64Data = Convert.ToBase64String(GetChart());
+            string imageDataURL = string.Format("data:image/png;base64,{0}", imageBase64Data);
+            if (imageDataURL.Length > 0)
+            {
+                ViewBag.ImageData = imageDataURL;
+            }
+            
             return View();
         }
 
-        public ActionResult About()
+        public byte[] GetChart()
         {
-            ViewBag.Message = "Your application description page.";
+            byte[] newChart = new Chart(width: 600, height: 400)
+            .AddTitle("Chart Title")
+            .AddSeries(
+                name: "Employee",
+                xValue: new[] { "Jeffrey", "Andrew", "Julie", "Mary", "Dave" },
+                yValues: new[] { "2", "6", "4", "5", "3" })
+            .GetBytes();
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return newChart;
         }
     }
 }
