9c51b87eaf70235772c84daadd96a02143c54d53
manvidas <manvidas@NM-16010-PC>
2018-10-23 08:19:58 +0100

    Initial commit

---
+++ GraphTestApplication2/GraphTestApplication2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GraphTestApplication2.Startup))]
+namespace GraphTestApplication2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
