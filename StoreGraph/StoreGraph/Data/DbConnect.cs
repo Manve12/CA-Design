503ec209ba589039a4b404cd4b393de1a98ba543
manvidas <manvidas@NM-16010-PC>
2018-10-24 09:26:21 +0100

    Added DbConnect class

---
+++ StoreGraph/StoreGraph/Data/DbConnect.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace StoreGraph.Data
+{
+    public static class DbConnect
+    {
+        public static string Connection { get; } = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public static SqlConnection ConnectionDatabase { get; set; } = new SqlConnection(Connection);
+
+        public static int ConnectionTimeout = 60;
+
+        public static void OpenConnection()
+        {
+            ConnectionDatabase.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            ConnectionDatabase.Close();
+        }
+    }
+}
