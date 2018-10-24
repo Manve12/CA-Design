69533f34ba8b5b10de659b34e87b34fb4fe6b5fe
manvidas <manvidas@NM-16010-PC>
2018-10-24 14:54:32 +0100

    Added store floor model

---
+++ StoreGraph/StoreGraph/Models/StoreFloorModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace StoreGraph.Models
+{
+    public static class StoreFloorModel
+    {
+        public static List<string> StoreFloors { get; set; }
+    }
+
+    public class StoreFloor
+    {
+        public string SelectedStoreFloor { get; set; }
+    }
+}
