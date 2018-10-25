25c01791ef3bcea312107c5ae946f5990156e7d5
manvidas <manvidas@NM-16010-PC>
2018-10-25 12:01:18 +0100

    Added buttons for total volume

---
+++ StoreGraph/StoreGraph/Models/TotalVolumeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreGraph.Models
+{
+    public static class TotalVolumeModel
+    {
+        public static List<string> TotalVolume { get; set; }
+        public static List<string> WeekCounter { get; set; }
+    }
+}
